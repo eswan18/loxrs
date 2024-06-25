69b417360510ac37ac6d3017df7e911c34ce2b57
Ethan Swan <eswan18@rocketmail.com>
2024-06-25 17:16:39 -0500

    Add some interpreting.

--- src/interpret/interpret.rs
+++ src/interpret/interpret.rs
@@ -1,5 +1,6 @@
 use crate::expr::Expr;
 use crate::interpret::RuntimeError;
+use crate::token::TokenType;
 use crate::value::LoxValue;
 
 /// Interpret the given expression.
@@ -13,7 +14,31 @@ pub fn interpret(expr: Expr) -> Result<LoxValue, RuntimeError> {
 fn eval(expr: Expr) -> Result<LoxValue, RuntimeError> {
     let evaluated = match expr {
         Expr::Literal { value } => LoxValue::new_from_literal(value),
+        Expr::Grouping { expression } => eval(*expression)?,
+        Expr::Unary { operator, right } => {
+            let right_val = eval(*right)?;
+            match operator.token_type {
+                TokenType::Minus => match right_val {
+                    LoxValue::Number(n) => LoxValue::Number(-n),
+                    _ => {
+                        return Err(RuntimeError::TypeError(
+                            "Operand to '-' must be a number".to_string(),
+                        ))
+                    }
+                },
+                TokenType::Bang => LoxValue::Boolean(!is_truthy(right_val)),
+                _ => unreachable!("Invalid unary operator"),
+            }
+        }
         _ => return Err(RuntimeError::Generic("Not implemented".to_string())),
     };
     Ok(evaluated)
+}
+
+fn is_truthy(value: LoxValue) -> bool {
+    match value {
+        LoxValue::Nil => false,
+        LoxValue::Boolean(b) => b,
+        _ => true,
+    }
 }
--- src/interpret/runtime_error.rs
+++ src/interpret/runtime_error.rs
@@ -3,12 +3,16 @@ use std::fmt;
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum RuntimeError {
     Generic(String),
+    TypeError(String),
 }
 
 impl fmt::Display for RuntimeError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             RuntimeError::Generic(msg) => {
+                write!(f, "{}", msg)
+            }
+            RuntimeError::TypeError(msg) => {
                 write!(f, "{}", msg)
             }
         }
